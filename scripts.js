18402a994c67fa567c43e6dfcff90b731c70b8b1
RisaruShop <161755588+RisaruShop@users.noreply.github.com>
2024-08-30 17:09:29 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-3
+function purchase(item) {
+    alert(item + " satın alındı!");
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mesajınız gönderildi!');
+});
